3f21e2f175162fc59f35321aaac8f10a1f738c1d
Jhballen <Jhballen@poligran.edu.co>
2020-10-10 19:23:06 -0500

    primer enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrearTweetComponent } from './crear-tweet/crear-tweet.component';
+import { RegistroUsuarioComponent } from './registro-usuario/registro-usuario.component';
+import { VerTweetsComponent } from './ver-tweets/ver-tweets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/crearUsuario',pathMatch: 'full'},
+  {path: 'crearTweet', component: CrearTweetComponent},
+  {path: 'crearUsuario', component: RegistroUsuarioComponent},
+  {path: 'verTweets', component: VerTweetsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
